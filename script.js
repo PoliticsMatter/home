abe9a2167126361326ce227baf275a42f6576cab
PoliticsMatter <30903711+PoliticsMatter@users.noreply.github.com>
2017-08-14 10:36:51 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-request.get({
-  url: "https://api.nytimes.com/svc/topstories/v2/national.json",
-  qs: {
-    'api-key': "457df9e90677458faa2e2763431d68a7"
-  },
-}, function(err, response, body) {
-  body = JSON.parse(body);
-  console.log(body);
-})
