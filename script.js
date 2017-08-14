860a210146fd4232f3c20628441ed3b6eba908a1
PoliticsMatter <30903711+PoliticsMatter@users.noreply.github.com>
2017-08-14 09:52:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-<script>
-var url = "https://api.nytimes.com/svc/topstories/v2/national.json";
-url += '?' + $.param({
-  'api-key': "457df9e90677458faa2e2763431d68a7"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
-</script>
+request.get({
+  url: "https://api.nytimes.com/svc/topstories/v2/national.json",
+  qs: {
+    'api-key': "457df9e90677458faa2e2763431d68a7"
+  },
+}, function(err, response, body) {
+  body = JSON.parse(body);
+  console.log(body);
+})
