97dfcfe29792627f6f1405eb1f4d8db4b73bd292
PoliticsMatter <30903711+PoliticsMatter@users.noreply.github.com>
2017-08-14 09:46:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<script>
 var url = "https://api.nytimes.com/svc/topstories/v2/national.json";
 url += '?' + $.param({
   'api-key': "457df9e90677458faa2e2763431d68a7"
@@ -10,3 +11,4 @@ $.ajax({
 }).fail(function(err) {
   throw err;
 });
+</script>
